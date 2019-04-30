428c02bf6b8672d3660adbabf5537abfb764b6e2
Lucas Lang <32493084+Llang8@users.noreply.github.com>
2019-04-29 22:31:16 -0500

    Added collision logic

--- monster-game.py
+++ monster-game.py
@@ -10,15 +10,20 @@ from os import system, name
 # import sleep to show output for some time period 
 from time import sleep 
   
-
+# Player object
 player = None
+# Board Object
 board = None
+
 # Handles button presses
 # Called when a button is pressed from the keyboard.on_press hook
 # Calls Player.move() and passes the direction to move
 def check_input(event):
+    # Pull in global variables
     global board
     global player
+
+    # Move character based on button pressed
     if event.name == 'up':
         try:
             player.moveCharacter(0,board)
@@ -40,7 +45,7 @@ def check_input(event):
         except:
             pass
 
-# define our clear function 
+# Clear function used to clear the terminal/console output 
 def clear(): 
   
     # for windows 
@@ -51,42 +56,59 @@ def clear():
     else: 
         _ = system('clear')
 
+# Returns a random number between 0 and the board size
 def randomPosition():
     global board
     return math.floor(random.random() * board.grid_size)
 
 # Main code loop
 def main():
+
+    # Pull in global variables
     global board
     global player
+
+    # Initialize board of size 4x4, fill with '+'
     board = Board(4,"+")
+    # Initialize Player object at position x=0 y=board size / 2
     player = Player(0,math.floor(board.grid_size/2))
+    # Initialize Egg object at random position
     egg = Egg(randomPosition(),randomPosition())
+    # Initialize Monster at random position
     monster = Monster(randomPosition(),randomPosition())
-    door = Door(randomPosition(),randomPosition())
+    # Initialize Door in last column middle row
+    door = Door(board.grid_size - 1, math.floor(board.grid_size/2))
+    # Add items to board
+    # TODO: Make this work in case of multiple eggs
     board.addItems([player,egg,monster,door])
     board.printGrid()
-    randomPosition()
-    for i in range(1,4):
-        while True:
-            clear()
-            board.printGrid()
-            player.printEggs()
-            if player.checkCollision(egg):
-                player.eggs+=1
-                # Once egg has been collected move off screen
-                egg.x = -1
-                egg.y = -1
-            if player.checkCollision(monster):
-                print("You lose!")
-                break
-            if player.checkCollision(door):
-                if player.eggs == board.eggs:
-                    print("You passed the level! Moving to the next...")
-                    sleep(2)
-                    break
-            sleep(.3) 
 
+    # Current level logic
+    while True:
+        # Clear console
+        clear()
+        # Print grid and egg count
+        board.printGrid()
+        player.printEggs()
+        # Check collisions with egg, if true increment eggs and remove egg from screen
+        if player.checkCollision(egg):
+            player.eggs+=1
+            # Once egg has been collected move off screen
+            egg.x = -1
+            egg.y = -1
+        # Check collisions with monster, if true player loses
+        if player.checkCollision(monster):
+            print("You lose!")
+            break
+        # Check collisions with door
+        if player.checkCollision(door):
+            # If player has collected all of the eggs move to next level
+            if player.eggs == board.eggs:
+                print("You passed the level! Moving to the next...")
+                sleep(2)
+                break
+        # Make code wait here to prevent refreshing too fast
+        sleep(.3)
 # This sets up a hook that listens for 
 keyboard.on_press(check_input)
 
